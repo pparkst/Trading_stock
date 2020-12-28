0bfeac9b35b6ec9f9436938637916a685e583f3e
pparkst@naver.com <ppark12@funshop.co.kr>
2020-12-28 23:42:33 +0900

    update: update common

--- src/slack/postMessage.py
+++ src/slack/postMessage.py
@@ -1,4 +1,14 @@
-from slacker import Slacker
-slack = Slacker('xoxb-1605937844820-1585025166151-y0szvrXCgW6aK94uWrr9lvHk')
+from trading import trading_object
+from datetime import datetime
+
+slack = trading_object.slack
 # Send a message to #general channel
 slack.chat.post_message('#test', 'Hello fellow slackers!')
+
+def postMessage(message):
+    """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
+    # 로컬에서만 파이썬 셀 출력
+    print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
+
+    strbuf = datetime.now().strftime('[%m/%d %H:%M:%S] ') + message
+    slack.chat.post_message('#test', strbuf)
