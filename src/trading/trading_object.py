7f812cf1055b13b6a052797e703d79b4a6716b38
pparkst@naver.com <ppark12@funshop.co.kr>
2020-12-28 23:44:22 +0900

    update: update Unittest

---
+++ src/trading/trading_object.py
@@ -0,0 +1,16 @@
+import win32com.client
+from slacker import Slacker
+
+# 크레온 플러스 공통 OBJECT
+cpCodeMgr = win32com.client.Dispatch('CpUtil.CpStockCode')
+cpStatus = win32com.client.Dispatch('CpUtil.CpCybos')
+cpTradeUtil = win32com.client.Dispatch('CpTrade.CpTdUtil')
+cpStock = win32com.client.Dispatch('DsCbo1.StockMst')
+cpOhlc = win32com.client.Dispatch('CpSysDib.StockChart')
+cpBalance = win32com.client.Dispatch('CpTrade.CpTd6033')
+cpCash = win32com.client.Dispatch('CpTrade.CpTdNew5331A')
+cpOrder = win32com.client.Dispatch('CpTrade.CpTd0311')  
+
+
+# git push ignore
+slack = Slacker('')
