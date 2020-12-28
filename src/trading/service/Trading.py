ddf7806790d59dd9c6c20ac10b4c9eb6e038a32f
pparkst@naver.com <ppark12@funshop.co.kr>
2020-12-28 23:44:45 +0900

    fixed: fix Trading Object

--- src/trading/service/Trading.py
+++ src/trading/service/Trading.py
@@ -2,33 +2,27 @@ import os, sys, ctypes
 import win32com.client
 import pandas as pd
 from datetime import datetime
-from slacker import Slacker
 import time, calendar
+#sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+import trading.trading_object as crObj
+from util.common import printlog
+from slack.postMessage import postMessage
+
 # from bs4 import BeautifulSoup
 # from urllib.request import urlopen
 # from selenium import webdriver
 # from selenium.webdriver.chrome.options import Options 
-
-slack = Slacker('xoxb-1605937844820-1605942367508-YQ9WVDH7pwCpFy2NiekoqdIi')
-def dbgout(message):
-    """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
-    print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
-    strbuf = datetime.now().strftime('[%m/%d %H:%M:%S] ') + message
-    slack.chat.post_message('#test', strbuf)
-
-def printlog(message, *args):
-    """인자로 받은 문자열을 파이썬 셸에 출력한다."""
-    print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message, *args)
  
-# 크레온 플러스 공통 OBJECT
-cpCodeMgr = win32com.client.Dispatch('CpUtil.CpStockCode')
-cpStatus = win32com.client.Dispatch('CpUtil.CpCybos')
-cpTradeUtil = win32com.client.Dispatch('CpTrade.CpTdUtil')
-cpStock = win32com.client.Dispatch('DsCbo1.StockMst')
-cpOhlc = win32com.client.Dispatch('CpSysDib.StockChart')
-cpBalance = win32com.client.Dispatch('CpTrade.CpTd6033')
-cpCash = win32com.client.Dispatch('CpTrade.CpTdNew5331A')
-cpOrder = win32com.client.Dispatch('CpTrade.CpTd0311')  
+# # 크레온 플러스 공통 OBJECT
+# cpCodeMgr = win32com.client.Dispatch('CpUtil.CpStockCode')
+# cpStatus = win32com.client.Dispatch('CpUtil.CpCybos')
+# cpTradeUtil = win32com.client.Dispatch('CpTrade.CpTdUtil')
+# cpStock = win32com.client.Dispatch('DsCbo1.StockMst')
+# cpOhlc = win32com.client.Dispatch('CpSysDib.StockChart')
+# cpBalance = win32com.client.Dispatch('CpTrade.CpTd6033')
+# cpCash = win32com.client.Dispatch('CpTrade.CpTdNew5331A')
+# cpOrder = win32com.client.Dispatch('CpTrade.CpTd0311')  
 
 def check_creon_system():
     """크레온 플러스 시스템 연결 상태를 점검한다."""
@@ -38,68 +32,69 @@ def check_creon_system():
         return False
  
     # 연결 여부 체크
-    if (cpStatus.IsConnect == 0):
+    if (crObj.cpStatus.IsConnect == 0):
         printlog('check_creon_system() : connect to server -> FAILED')
         return False
  
     # 주문 관련 초기화 - 계좌 관련 코드가 있을 때만 사용
-    if (cpTradeUtil.TradeInit(0) != 0):
+    if (crObj.cpTradeUtil.TradeInit(0) != 0):
         printlog('check_creon_system() : init trade -> FAILED')
         return False
+
     return True
 
 def get_current_price(code):
     """인자로 받은 종목의 현재가, 매수호가, 매도호가를 반환한다."""
-    cpStock.SetInputValue(0, code)  # 종목코드에 대한 가격 정보
-    cpStock.BlockRequest()
+    crObj.cpStock.SetInputValue(0, code)  # 종목코드에 대한 가격 정보
+    crObj.cpStock.BlockRequest()
     item = {}
-    item['cur_price'] = cpStock.GetHeaderValue(11)   # 현재가
-    item['ask'] =  cpStock.GetHeaderValue(16)        # 매수호가
-    item['bid'] =  cpStock.GetHeaderValue(17)        # 매도호가    
+    item['cur_price'] = crObj.cpStock.GetHeaderValue(11)   # 현재가
+    item['ask'] =  crObj.cpStock.GetHeaderValue(16)        # 매수호가
+    item['bid'] =  crObj.cpStock.GetHeaderValue(17)        # 매도호가    
     return item['cur_price'], item['ask'], item['bid']
 
 def get_ohlc(code, qty):
     """인자로 받은 종목의 OHLC 가격 정보를 qty 개수만큼 반환한다."""
-    cpOhlc.SetInputValue(0, code)           # 종목코드
-    cpOhlc.SetInputValue(1, ord('2'))        # 1:기간, 2:개수
-    cpOhlc.SetInputValue(4, qty)             # 요청개수
-    cpOhlc.SetInputValue(5, [0, 2, 3, 4, 5]) # 0:날짜, 2~5:OHLC
-    cpOhlc.SetInputValue(6, ord('D'))        # D:일단위
-    cpOhlc.SetInputValue(9, ord('1'))        # 0:무수정주가, 1:수정주가
-    cpOhlc.BlockRequest()
-    count = cpOhlc.GetHeaderValue(3)   # 3:수신개수
+    crObj.cpOhlc.SetInputValue(0, code)           # 종목코드
+    crObj.cpOhlc.SetInputValue(1, ord('2'))        # 1:기간, 2:개수
+    crObj.cpOhlc.SetInputValue(4, qty)             # 요청개수
+    crObj.cpOhlc.SetInputValue(5, [0, 2, 3, 4, 5]) # 0:날짜, 2~5:OHLC
+    crObj.cpOhlc.SetInputValue(6, ord('D'))        # D:일단위
+    crObj.cpOhlc.SetInputValue(9, ord('1'))        # 0:무수정주가, 1:수정주가
+    crObj.cpOhlc.BlockRequest()
+    count = crObj.cpOhlc.GetHeaderValue(3)   # 3:수신개수
     columns = ['open', 'high', 'low', 'close']
     index = []
     rows = []
     for i in range(count): 
-        index.append(cpOhlc.GetDataValue(0, i)) 
-        rows.append([cpOhlc.GetDataValue(1, i), cpOhlc.GetDataValue(2, i),
-            cpOhlc.GetDataValue(3, i), cpOhlc.GetDataValue(4, i)]) 
+        index.append(crObj.cpOhlc.GetDataValue(0, i)) 
+        rows.append([crObj.cpOhlc.GetDataValue(1, i), crObj.cpOhlc.GetDataValue(2, i),
+            crObj.cpOhlc.GetDataValue(3, i), crObj.cpOhlc.GetDataValue(4, i)]) 
     df = pd.DataFrame(rows, columns=columns, index=index) 
     return df
 
 def get_stock_balance(code):
     """인자로 받은 종목의 종목명과 수량을 반환한다."""
-    cpTradeUtil.TradeInit()
-    acc = cpTradeUtil.AccountNumber[0]      # 계좌번호
-    accFlag = cpTradeUtil.GoodsList(acc, 1) # -1:전체, 1:주식, 2:선물/옵션
-    cpBalance.SetInputValue(0, acc)         # 계좌번호
-    cpBalance.SetInputValue(1, accFlag[0])  # 상품구분 - 주식 상품 중 첫번째
-    cpBalance.SetInputValue(2, 50)          # 요청 건수(최대 50)
-    cpBalance.BlockRequest()     
+    crObj.cpTradeUtil.TradeInit()
+    acc = crObj.cpTradeUtil.AccountNumber[0]      # 계좌번호
+    accFlag = crObj.cpTradeUtil.GoodsList(acc, 1) # -1:전체, 1:주식, 2:선물/옵션
+    crObj.cpBalance.SetInputValue(0, acc)         # 계좌번호
+    crObj.cpBalance.SetInputValue(1, accFlag[0])  # 상품구분 - 주식 상품 중 첫번째
+    crObj.cpBalance.SetInputValue(2, 50)          # 요청 건수(최대 50)
+    crObj.cpBalance.BlockRequest()     
     if code == 'ALL':
-        dbgout('계좌명: ' + str(cpBalance.GetHeaderValue(0)))
-        dbgout('결제잔고수량 : ' + str(cpBalance.GetHeaderValue(1)))
-        dbgout('평가금액: ' + str(cpBalance.GetHeaderValue(3)))
-        dbgout('평가손익: ' + str(cpBalance.GetHeaderValue(4)))
-        dbgout('종목수: ' + str(cpBalance.GetHeaderValue(7)))
+        postMessage('계좌명: ' + str(crObj.cpBalance.GetHeaderValue(0)))
+        postMessage('결제잔고수량 : ' + str(crObj.cpBalance.GetHeaderValue(1)))
+        postMessage('평가금액: ' + str(crObj.cpBalance.GetHeaderValue(3)))
+        postMessage('평가손익: ' + str(crObj.cpBalance.GetHeaderValue(4)))
+        postMessage('종목수: ' + str(crObj.cpBalance.GetHeaderValue(7)))
     stocks = []
-    for i in range(cpBalance.GetHeaderValue(7)):
-        stock_code = cpBalance.GetDataValue(12, i)  # 종목코드
-        stock_name = cpBalance.GetDataValue(0, i)   # 종목명
-        stock_qty = cpBalance.GetDataValue(15, i)   # 수량
+    for i in range(crObj.cpBalance.GetHeaderValue(7)):
+        stock_code = crObj.cpBalance.GetDataValue(12, i)  # 종목코드
+        stock_name = crObj.cpBalance.GetDataValue(0, i)   # 종목명
+        stock_qty = crObj.cpBalance.GetDataValue(15, i)   # 수량
         if code == 'ALL':
-            dbgout(str(i+1) + ' ' + stock_code + '(' + stock_name + ')' 
+            postMessage(str(i+1) + ' ' + stock_code + '(' + stock_name + ')' 
                 + ':' + str(stock_qty))
             stocks.append({'code': stock_code, 'name': stock_name, 
                 'qty': stock_qty})
@@ -108,18 +103,18 @@ def get_stock_balance(code):
     if code == 'ALL':
         return stocks
     else:
-        stock_name = cpCodeMgr.CodeToName(code)
+        stock_name = crObj.cpCodeMgr.CodeToName(code)
         return stock_name, 0
 
 def get_current_cash():
     """증거금 100% 주문 가능 금액을 반환한다."""
-    cpTradeUtil.TradeInit()
-    acc = cpTradeUtil.AccountNumber[0]    # 계좌번호
-    accFlag = cpTradeUtil.GoodsList(acc, 1) # -1:전체, 1:주식, 2:선물/옵션
-    cpCash.SetInputValue(0, acc)              # 계좌번호
-    cpCash.SetInputValue(1, accFlag[0])      # 상품구분 - 주식 상품 중 첫번째
-    cpCash.BlockRequest() 
-    return cpCash.GetHeaderValue(9) # 증거금 100% 주문 가능 금액
+    crObj.cpTradeUtil.TradeInit()
+    acc = crObj.cpTradeUtil.AccountNumber[0]    # 계좌번호
+    accFlag = crObj.cpTradeUtil.GoodsList(acc, 1) # -1:전체, 1:주식, 2:선물/옵션
+    crObj.cpCash.SetInputValue(0, acc)              # 계좌번호
+    crObj.cpCash.SetInputValue(1, accFlag[0])      # 상품구분 - 주식 상품 중 첫번째
+    crObj.cpCash.BlockRequest() 
+    return crObj.cpCash.GetHeaderValue(9) # 증거금 100% 주문 가능 금액
 
 def get_target_price(code):
     """매수 목표가를 반환한다."""
@@ -138,7 +133,7 @@ def get_target_price(code):
         target_price = today_open + (lastday_high - lastday_low) * 0.5
         return target_price
     except Exception as ex:
-        dbgout("`get_target_price() -> exception! " + str(ex) + "`")
+        postMessage("`get_target_price() -> exception! " + str(ex) + "`")
         return None
     
 def get_movingaverage(code, window):
@@ -155,7 +150,7 @@ def get_movingaverage(code, window):
         ma = closes.rolling(window=window).mean()
         return ma.loc[lastday]
     except Exception as ex:
-        dbgout('get_movingavrg(' + str(window) + ') -> exception! ' + str(ex))
+        postMessage('get_movingavrg(' + str(window) + ') -> exception! ' + str(ex))
         return None    
 
 def buy_etf(code):
@@ -180,23 +175,23 @@ def buy_etf(code):
             and current_price > ma10_price:  
             printlog(stock_name + '(' + str(code) + ') ' + str(buy_qty) +
                 'EA : ' + str(current_price) + ' meets the buy condition!`')            
-            cpTradeUtil.TradeInit()
-            acc = cpTradeUtil.AccountNumber[0]      # 계좌번호
-            accFlag = cpTradeUtil.GoodsList(acc, 1) # -1:전체,1:주식,2:선물/옵션                
+            crObj.cpTradeUtil.TradeInit()
+            acc = crObj.cpTradeUtil.AccountNumber[0]      # 계좌번호
+            accFlag = crObj.cpTradeUtil.GoodsList(acc, 1) # -1:전체,1:주식,2:선물/옵션                
             # 최유리 FOK 매수 주문 설정
-            cpOrder.SetInputValue(0, "2")        # 2: 매수
-            cpOrder.SetInputValue(1, acc)        # 계좌번호
-            cpOrder.SetInputValue(2, accFlag[0]) # 상품구분 - 주식 상품 중 첫번째
-            cpOrder.SetInputValue(3, code)       # 종목코드
-            cpOrder.SetInputValue(4, buy_qty)    # 매수할 수량
-            cpOrder.SetInputValue(7, "2")        # 주문조건 0:기본, 1:IOC, 2:FOK
-            cpOrder.SetInputValue(8, "12")       # 주문호가 1:보통, 3:시장가
+            crObj.cpOrder.SetInputValue(0, "2")        # 2: 매수
+            crObj.cpOrder.SetInputValue(1, acc)        # 계좌번호
+            crObj.cpOrder.SetInputValue(2, accFlag[0]) # 상품구분 - 주식 상품 중 첫번째
+            crObj.cpOrder.SetInputValue(3, code)       # 종목코드
+            crObj.cpOrder.SetInputValue(4, buy_qty)    # 매수할 수량
+            crObj.cpOrder.SetInputValue(7, "2")        # 주문조건 0:기본, 1:IOC, 2:FOK
+            crObj.cpOrder.SetInputValue(8, "12")       # 주문호가 1:보통, 3:시장가
                                                  # 5:조건부, 12:최유리, 13:최우선 
             # 매수 주문 요청
-            ret = cpOrder.BlockRequest() 
+            ret = crObj.cpOrder.BlockRequest() 
             printlog('최유리 FoK 매수 ->', stock_name, code, buy_qty, '->', ret)
             if ret == 4:
-                remain_time = cpStatus.LimitRequestRemainTime
+                remain_time = crObj.cpStatus.LimitRequestRemainTime
                 printlog('주의: 연속 주문 제한에 걸림. 대기 시간:', remain_time/1000)
                 time.sleep(remain_time/1000) 
                 return False
@@ -206,17 +201,17 @@ def buy_etf(code):
             printlog('get_stock_balance :', stock_name, stock_qty)
             if bought_qty > 0:
                 bought_list.append(code)
-                dbgout("`buy_etf("+ str(stock_name) + ' : ' + str(code) + 
+                postMessage("`buy_etf("+ str(stock_name) + ' : ' + str(code) + 
                     ") -> " + str(bought_qty) + "EA bought!" + "`")
     except Exception as ex:
-        dbgout("`buy_etf("+ str(code) + ") -> exception! " + str(ex) + "`")
+        postMessage("`buy_etf("+ str(code) + ") -> exception! " + str(ex) + "`")
 
 def sell_all():
     """보유한 모든 종목을 최유리 지정가 IOC 조건으로 매도한다."""
     try:
-        cpTradeUtil.TradeInit()
-        acc = cpTradeUtil.AccountNumber[0]       # 계좌번호
-        accFlag = cpTradeUtil.GoodsList(acc, 1)  # -1:전체, 1:주식, 2:선물/옵션   
+        crObj.cpTradeUtil.TradeInit()
+        acc = crObj.cpTradeUtil.AccountNumber[0]       # 계좌번호
+        accFlag = crObj.cpTradeUtil.GoodsList(acc, 1)  # -1:전체, 1:주식, 2:선물/옵션   
         while True:    
             stocks = get_stock_balance('ALL') 
             total_qty = 0 
@@ -226,28 +221,28 @@ def sell_all():
                 return True
             for s in stocks:
                 if s['qty'] != 0:                  
-                    cpOrder.SetInputValue(0, "1")         # 1:매도, 2:매수
-                    cpOrder.SetInputValue(1, acc)         # 계좌번호
-                    cpOrder.SetInputValue(2, accFlag[0])  # 주식상품 중 첫번째
-                    cpOrder.SetInputValue(3, s['code'])   # 종목코드
-                    cpOrder.SetInputValue(4, s['qty'])    # 매도수량
-                    cpOrder.SetInputValue(7, "1")   # 조건 0:기본, 1:IOC, 2:FOK
-                    cpOrder.SetInputValue(8, "12")  # 호가 12:최유리, 13:최우선 
+                    crObj.cpOrder.SetInputValue(0, "1")         # 1:매도, 2:매수
+                    crObj.cpOrder.SetInputValue(1, acc)         # 계좌번호
+                    crObj.cpOrder.SetInputValue(2, accFlag[0])  # 주식상품 중 첫번째
+                    crObj.cpOrder.SetInputValue(3, s['code'])   # 종목코드
+                    crObj.cpOrder.SetInputValue(4, s['qty'])    # 매도수량
+                    crObj.cpOrder.SetInputValue(7, "1")   # 조건 0:기본, 1:IOC, 2:FOK
+                    crObj.cpOrder.SetInputValue(8, "12")  # 호가 12:최유리, 13:최우선 
                     # 최유리 IOC 매도 주문 요청
-                    ret = cpOrder.BlockRequest()
+                    ret = crObj.cpOrder.BlockRequest()
                     printlog('최유리 IOC 매도', s['code'], s['name'], s['qty'], 
                         '-> cpOrder.BlockRequest() -> returned', ret)
                     if ret == 4:
-                        remain_time = cpStatus.LimitRequestRemainTime
+                        remain_time = crObj.cpStatus.LimitRequestRemainTime
                         printlog('주의: 연속 주문 제한, 대기시간:', remain_time/1000)
                 time.sleep(1)
             time.sleep(30)
     except Exception as ex:
-        dbgout("sell_all() -> exception! " + str(ex))
+        postMessage("sell_all() -> exception! " + str(ex))
 
 if __name__ == '__main__': 
     try:
-        symbol_list = ['']
+        symbol_list = ['종목 뭐 넣을까 으음']
         bought_list = []     # 매수 완료된 종목 리스트
         target_buy_count = 5 # 매수할 종목 수
         buy_percent = 0.25   
@@ -284,11 +279,11 @@ if __name__ == '__main__':
                     time.sleep(5)
             if t_sell < t_now < t_exit:  # PM 03:15 ~ PM 03:20 : 일괄 매도
                 if sell_all() == True:
-                    dbgout('`sell_all() returned True -> self-destructed!`')
+                    postMessage('`sell_all() returned True -> self-destructed!`')
                     sys.exit(0)
             if t_exit < t_now:  # PM 03:20 ~ :프로그램 종료
-                dbgout('`self-destructed!`')
+                postMessage('`self-destructed!`')
                 sys.exit(0)
             time.sleep(3)
     except Exception as ex:
-        dbgout('`main -> exception! ' + str(ex) + '`')
+        postMessage('`main -> exception! ' + str(ex) + '`')
