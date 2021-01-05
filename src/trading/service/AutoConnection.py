c14cb9a138acf8b68cbb816f7b69abdf8369d788
pparkst <pparkst@naver.com>
2021-01-05 23:16:23 +0900

    init: PersonalData

--- src/trading/service/AutoConnection.py
+++ src/trading/service/AutoConnection.py
@@ -1,6 +1,9 @@
 from pywinauto import application
 import time
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
+import PersonalData as pD
 
 def AutoConnectionCreon():
     os.system('taskkill /IM coStarter* /F /T')
@@ -10,5 +13,7 @@ def AutoConnectionCreon():
     time.sleep(5)        
 
     app = application.Application()
-    app.start('C:\CREON\STARTER\coStarter.exe /prj:cp /id: /pwd: /pwdcert: /autostart')
+    app.start('C:\CREON\STARTER\coStarter.exe%s/autostart' % pD.accountInfo)
     time.sleep(60)
+
+AutoConnectionCreon()
